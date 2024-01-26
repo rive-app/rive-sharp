ce2f3483a0653c161268053307013affa36c1a4f
luigi-rosso <hello@rive.app>
2024-01-26 23:52:54 +0000

    Lua formatter
    Format all our lua scripts with Stylua
    
    https://www.notion.so/rive-app/Formatters-be8845abbca34e39b5d4c212b2437b3f?pvs=4
    
    Diffs=
    858215cc4 Lua formatter (#6525)
    
    Co-authored-by: Luigi Rosso <luigi-rosso@users.noreply.github.com>

--- native/premake5.lua
+++ native/premake5.lua
@@ -1,60 +1,54 @@
-workspace "rive-cpp"
+workspace('rive-cpp')
 
-configurations {"Debug", "Release"}
-platforms {"x64", "x86", "ARM64", "ARM"}
+configurations({ 'Debug', 'Release' })
+platforms({ 'x64', 'x86', 'ARM64', 'ARM' })
 
 -- Are we in the "rive-sharp" or "rive" repository?
-local handle = io.popen("git remote -v")
-local git_remote = handle:read("*a")
+local handle = io.popen('git remote -v')
+local git_remote = handle:read('*a')
 handle:close()
-if string.find(git_remote, "rive%-sharp") then
+if string.find(git_remote, 'rive%-sharp') then
     -- In rive-sharp. Rive runtime is a submodule.
-    RIVE_RUNTIME_DIR = "../submodules/rive-cpp"
+    RIVE_RUNTIME_DIR = '../submodules/rive-cpp'
 else
     -- In rive. Rive runtime is further up the tree.
-    RIVE_RUNTIME_DIR = "../../runtime"
+    RIVE_RUNTIME_DIR = '../../runtime'
 end
 
-project "rive"
-    kind "SharedLib"
-    language "C++"
-    cppdialect "C++17"
-    targetdir "bin/%{cfg.platform}/%{cfg.buildcfg}"
-    objdir "obj/%{cfg.platform}/%{cfg.buildcfg}"
-    staticruntime "off"  -- /MD for dll
-    flags { "FatalWarnings" }
-    includedirs {
-        RIVE_RUNTIME_DIR .. "/include",
-        "../../include",
-    }
-    files {
-        RIVE_RUNTIME_DIR .. "/src/**.cpp",
-        "RiveSharpInterop.cpp"
-    }
+project('rive')
+kind('SharedLib')
+language('C++')
+cppdialect('C++17')
+targetdir('bin/%{cfg.platform}/%{cfg.buildcfg}')
+objdir('obj/%{cfg.platform}/%{cfg.buildcfg}')
+staticruntime('off') -- /MD for dll
+flags({ 'FatalWarnings' })
+includedirs({ RIVE_RUNTIME_DIR .. '/include', '../../include' })
+files({ RIVE_RUNTIME_DIR .. '/src/**.cpp', 'RiveSharpInterop.cpp' })
 
-    filter "configurations:Debug"
-        defines {"DEBUG"}
-        symbols "On"
+filter('configurations:Debug')
+defines({ 'DEBUG' })
+symbols('On')
 
-    filter "configurations:Release"
-        defines {"RELEASE"}
-        defines {"NDEBUG"}
-        optimize "Size"
+filter('configurations:Release')
+defines({ 'RELEASE' })
+defines({ 'NDEBUG' })
+optimize('Size')
 
-    filter "platforms:x64"
-        architecture "x64"
-        toolset "clang"
+filter('platforms:x64')
+architecture('x64')
+toolset('clang')
 
-    filter "platforms:x86"
-        architecture "x86"
-        toolset "clang"
+filter('platforms:x86')
+architecture('x86')
+toolset('clang')
 
-    filter "platforms:ARM64"
-        architecture "ARM64"
-        toolset "clang"
+filter('platforms:ARM64')
+architecture('ARM64')
+toolset('clang')
 
-    filter "platforms:ARM"
-        architecture "ARM"
-        toolset "msc" -- clang isn't supported on ARM32
+filter('platforms:ARM')
+architecture('ARM')
+toolset('msc') -- clang isn't supported on ARM32
 
-    filter {}
+filter({})
--- native/premake5_wasm.lua
+++ native/premake5_wasm.lua
@@ -1,55 +1,47 @@
-workspace "rive-cpp"
+workspace('rive-cpp')
 
-configurations {"Release"}
+configurations({ 'Release' })
 
 -- Are we in the "rive-sharp" or "rive" repository?
-local git_handle = io.popen("git remote -v")
-local git_remote = git_handle:read("*a")
+local git_handle = io.popen('git remote -v')
+local git_remote = git_handle:read('*a')
 git_handle:close()
-if string.find(git_remote, "rive%-sharp") then
+if string.find(git_remote, 'rive%-sharp') then
     -- In rive-sharp. Rive runtime is a submodule.
-    RIVE_RUNTIME_DIR = "../submodules/rive-cpp"
+    RIVE_RUNTIME_DIR = '../submodules/rive-cpp'
 else
     -- In rive. Rive runtime is further up the tree.
-    RIVE_RUNTIME_DIR = "../../runtime"
+    RIVE_RUNTIME_DIR = '../../runtime'
 end
 
-local emcc_handle = io.popen("emcc --version | grep ^emcc | sed 's/^.*\\([0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\\).*$/\\1/'")
-local emcc_version = emcc_handle:read("*a"):gsub("%s+", "")
+local emcc_handle = io.popen(
+    'emcc --version | grep ^emcc | sed \'s/^.*\\([0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\\).*$/\\1/\''
+)
+local emcc_version = emcc_handle:read('*a'):gsub('%s+', '')
 emcc_handle:close()
-TARGET_DIR = "bin/emsdk/" .. emcc_version
-print("TARGET_DIR: " .. TARGET_DIR)
+TARGET_DIR = 'bin/emsdk/' .. emcc_version
+print('TARGET_DIR: ' .. TARGET_DIR)
 
-project "rive"
+project('rive')
 do
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++17"
-    targetdir (TARGET_DIR)
-    objdir "obj/wasm"
-    optimize "Size"
-    flags { "FatalWarnings" }
-    includedirs {
-        RIVE_RUNTIME_DIR .. "/include",
-        "../../include",
-    }
-    files {
-        RIVE_RUNTIME_DIR .. "/src/**.cpp",
-        "RiveSharpInterop.cpp"
-    }
-    defines {
-        "RELEASE",
-        "NDEBUG",
-        "WASM"
-    }
-    filter "options:not no-exceptions"
+    kind('StaticLib')
+    language('C++')
+    cppdialect('C++17')
+    targetdir(TARGET_DIR)
+    objdir('obj/wasm')
+    optimize('Size')
+    flags({ 'FatalWarnings' })
+    includedirs({ RIVE_RUNTIME_DIR .. '/include', '../../include' })
+    files({ RIVE_RUNTIME_DIR .. '/src/**.cpp', 'RiveSharpInterop.cpp' })
+    defines({ 'RELEASE', 'NDEBUG', 'WASM' })
+    filter('options:not no-exceptions')
     do
         -- WebAssembly Exceptions support is now required by Uno.
-        buildoptions { "-fwasm-exceptions" }
+        buildoptions({ '-fwasm-exceptions' })
     end
 end
 
-newoption {
+newoption({
     trigger = 'no-exceptions',
     description = 'build without -fwasm-exceptions',
-}
+})
